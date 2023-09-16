a78ade9b0df71863ffa8918b74c278cec79dea04
facu ierullo <ierullofacu@gmail.com>
2023-09-16 15:59:41 -0300

    final

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 function Footer () {
     return(
-        <footer className="text-center text-bg-dark fs-5"> Facundo Ierullo 2023 © </footer>
+        <footer className="text-center text-bg-dark fs-5"> Copyright © 2023 | Facundo Ierullo  </footer>
         )
 }
 
